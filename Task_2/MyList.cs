5bbcb879e606d18004157307d943bd375d0b6d89
Artem Priadchenko <yorgan.info@gmail.com>
2023-05-18 00:11:41 +0300

    Добавьте файлы проекта.

---
+++ Task_2/MyList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+
+    //Мінімально необхідний інтерфейс взаємодії:
+    //метод додавання елемента, 
+    //індексатор для отримання значення елемента за вказаним індексом 
+    //властивість тільки для читання для отримання загальної кількості елементів.
+
+
+    public class MyList<T>
+    {
+
+        private T[] _items;
+        
+        public int Count
+        {
+            get { return _items.Length; }
+        }
+
+        public T this[int intdex]
+        {
+            get { return _items[intdex]; }
+        }
+
+
+        public void Add(T item)
+        {
+            if (_items == null)
+            {
+                _items = new T[0];
+            }
+         
+
+            T[] tempItems = new T[_items.Length +1];
+
+            for (int i = 0; i < _items.Length; i++)
+            {
+                tempItems[i] = _items[i];
+            }
+
+            tempItems[_items.Length] = item;
+
+            _items = tempItems;
+        }
+
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<int> list = new MyList<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"Значення елемента MyList: {list[i]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
