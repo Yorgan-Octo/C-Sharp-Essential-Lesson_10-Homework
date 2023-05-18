ef4b34535fe8187852ecb9924a110aebdbc7b588
Artem Pr <yorgan.info@gmail.com>
2023-05-18 12:08:39 +0300

    Завднання 4

---
+++ Task_4/Extension/MyListExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_2;
+
+namespace Task_4
+{
+    public static class MyListExtension
+    {
+        public static T[] GetArray<T>(this MyList<T> list)
+        {
+            T[] array = new T[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                array[i] = list[i];
+            }
+            return array;
+        }
+
+    }
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_2;
+
+namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> list = new MyList<string>();
+
+            list.Add("sdfff");
+            list.Add("uyi");
+            list.Add("ghjk");
+            list.Add("dgg");
+            list.Add("ghjk");
+            list.Add("uyuuu");
+            list.Add("dtee");
+            list.Add("wsew");
+            list.Add("ewss");
+
+            string[] elements = list.GetArray();
+
+            foreach (var item in elements)
+            {
+                Console.WriteLine("MyListExtension: " + item);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
