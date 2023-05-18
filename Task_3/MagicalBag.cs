4a8f151f134bafd8977e18d683c2835320c997c2
Artem Priadchenko <yorgan.info@gmail.com>
2023-05-18 23:46:53 +0300

    Завдання 3 ночало

---
+++ Task_3/MagicalBag.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    public class MagicalBag<T> where T : IEnimal
+    {
+
+        List<string> elfeGifts = new List<string>()
+        {
+            "elfeGifts Подарок 1",
+            "elfeGifts Подарок 2",
+            "elfeGifts Подарок 3",
+            "elfeGifts Подарок 4",
+            "elfeGifts Подарок 5",
+        };
+
+        List<string> humanGifts = new List<string>()
+        {
+            "humanGifts Подарок 1",
+            "humanGifts Подарок 2",
+            "humanGifts Подарок 3",
+            "humanGifts Подарок 4",
+            "humanGifts Подарок 5",
+        };
+
+
+
+
+    }
+}
