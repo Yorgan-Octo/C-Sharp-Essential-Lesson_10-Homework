f687bbfa404b75e9ff4afef2d458e8a85b4a03d5
Artem Pr <yorgan.info@gmail.com>
2023-05-19 16:31:52 +0300

    Завадання 3 вар3

---
+++ Task_3-v3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_v3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
